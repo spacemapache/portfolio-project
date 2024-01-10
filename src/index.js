10260b0a808fbca727e31430c7d818402a99f6c1
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-01-10 15:21:49 -0800

    added javascript for contact form, edited css styling

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function submitMessage() {
+  alert(`Thank you for your message! I'll be in touch soon📮`);
+}
+
+let submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", submitMessage);
